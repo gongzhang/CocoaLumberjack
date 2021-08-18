6d7736a511e535f410865cc26298bf7887b4f49e
Gong Zhang <zhanggong@me.com>
2021-08-18 19:53:27 +0800

    Fix build in Xcode 13 beta 5

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "CocoaLumberjack",
     platforms: [
-        .iOS(.v9),
-        .macOS(.v10_10),
-        .watchOS(.v3),
-        .tvOS(.v9),
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v7),
+        .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
